d4096b950b3507ea44a6042b59a4311d59bba10c
Ken Morton Jr <kmorton15@gmail.com>
2020-03-08 23:51:54 -0400

    added map showing nerb

--- components/NerbInfo.vue
+++ components/NerbInfo.vue
@@ -37,10 +37,16 @@
             </a>
           </section>
         </div>
+
         <div class="info column is-half-desktop has-text-centered">
-          <section class="section">
-            <p>Map here</p>
-          </section>
+          <figure class="image is-5by4">
+            <iframe
+              class="has-ratio"
+              src="https://maps.google.com/maps?q=New%20England%20Roast%20Beef&t=&z=15&ie=UTF8&iwloc=&output=embed"
+              frameborder="0"
+              allowfullscreen
+            ></iframe>
+          </figure>
         </div>
       </div>
     </div>

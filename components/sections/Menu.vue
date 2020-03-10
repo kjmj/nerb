0cb2c8d5c25226be3d78d9214f7eecf0dd6a2cd2
Ken Morton Jr <kmorton15@gmail.com>
2020-03-09 20:13:33 -0400

    revamp of how menu is constructed to allow better use of whitespace. also added all menu items

--- components/sections/Menu.vue
+++ components/sections/Menu.vue
@@ -7,50 +7,23 @@
     </section>
     <div class="container menuContainer">
       <section class="section">
+        <!--The idea is to create two columns-->
+        <!--We fill the columns with "MenuSection" components, each representing a section of the menu (i.e. Appetizers, Sandwiches, Desserts)-->
+        <!--This way, there is no vertical space wasted between row elements in the columns-->
         <div class="columns is-desktop is-multiline">
-          <div
-            v-for="category in menu"
-            :key="category.title"
-            class="column is-half-desktop has-text-centered"
-          >
-            <p class="is-size-4 stripes">
-              <span class="bb">
-                {{ category.title }}
-              </span>
-            </p>
-            <p class="is-size-6">{{ category.desc }}</p>
-            <br />
-            <div class="menuItem">
-              <div class="mainLine">
-                <span class="menuColumns"></span>
-                <span
-                  v-for="(col, colIndex) in category.menuColumns"
-                  :key="colIndex"
-                  class="price is-size-6"
-                >
-                  {{ col }}</span
-                >
-              </div>
-            </div>
-            <div
-              v-for="menuItem in category.items"
-              :key="menuItem.name"
-              class="menuItem"
-            >
-              <div class="mainLine is-size-6">
-                <span class="name">{{ menuItem.name }}</span>
-                <span
-                  v-for="(price, priceIndex) in menuItem.prices"
-                  :key="priceIndex"
-                  class="price is-size-6"
-                >
-                  {{ price }}
-                </span>
-              </div>
-              <div class="itemDescription is-size-6">
-                {{ menuItem.desc }}
-              </div>
-            </div>
+          <div class="column is-half-desktop has-text-centered">
+            <MenuSection
+              v-for="section in first"
+              :key="section.title"
+              :section="section"
+            ></MenuSection>
+          </div>
+          <div class="column is-half-desktop has-text-centered">
+            <MenuSection
+              v-for="section in second"
+              :key="section.title"
+              :section="section"
+            ></MenuSection>
           </div>
         </div>
       </section>
@@ -59,58 +32,27 @@
 </template>
 
 <script>
+import MenuSection from '../uiElements/MenuSection'
 import nerbMenu from './nerbMenu'
 
 export default {
   name: 'Menu',
+  components: { MenuSection },
   data() {
     return {
-      menu: nerbMenu.menu
+      first: nerbMenu.menu.firstHalf,
+      second: nerbMenu.menu.secondHalf
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-@import '../../static/style';
-
 .menuContainer {
   width: 80%;
   margin-top: -150px;
   background-color: white;
   z-index: 0;
-}
-
-.mainLine {
-  display: flex;
-}
-
-.name,
-.menuColumns {
-  margin-right: 0.5em;
-  flex: 1;
-  display: flex;
-}
-
-.name:after {
-  content: '';
-  border-bottom: dotted 1px;
-  flex: 1;
-}
-
-.menuColumns:after {
-  content: '';
-  flex: 1;
-}
-
-.price {
-  text-align: right;
-  flex: 0 0 4em;
-}
-.itemDescription {
-  opacity: 0.8;
-  margin-bottom: 10px;
-  text-align: left;
 }
 .has-bg-img {
   background: url('https://shop.guglhupf.com/images/products/kaiser.jpg') center
---
+++ components/uiElements/MenuSection.vue
@@ -0,0 +1,94 @@
+<!--This class represents a single section in the menu component-->
+<template>
+  <div>
+    <p class="is-size-4 stripes">
+      <span class="bb">
+        <b>{{ section.title }}</b>
+      </span>
+    </p>
+    <p class="is-size-6">{{ section.desc }}</p>
+    <br />
+
+    <div class="menuItem">
+      <div class="mainLine">
+        <span class="menuColumns"></span>
+        <span
+          v-for="(col, colIndex) in section.menuColumns"
+          :key="colIndex"
+          class="price is-size-6"
+        >
+          {{ col }}</span
+        >
+      </div>
+    </div>
+
+    <div
+      v-for="menuItem in section.items"
+      :key="menuItem.name"
+      class="menuItem"
+    >
+      <div class="mainLine is-size-6">
+        <span class="name">{{ menuItem.name }}</span>
+        <span
+          v-for="(price, priceIndex) in menuItem.prices"
+          :key="priceIndex"
+          class="price is-size-6"
+        >
+          {{ price }}
+        </span>
+      </div>
+      <div class="itemDescription is-size-6">
+        {{ menuItem.desc }}
+      </div>
+    </div>
+    <br />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuSection',
+  props: {
+    section: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../../static/style';
+
+.mainLine {
+  display: flex;
+}
+
+.name,
+.menuColumns {
+  margin-right: 0.5em;
+  flex: 1;
+  display: flex;
+}
+
+.name:after {
+  content: '';
+  border-bottom: dotted 1px;
+  flex: 1;
+}
+
+.menuColumns:after {
+  content: '';
+  flex: 1;
+}
+
+.price {
+  text-align: right;
+  flex: 0 0 4em;
+}
+.itemDescription {
+  opacity: 0.8;
+  margin-bottom: 10px;
+  text-align: left;
+}
+</style>

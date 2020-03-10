ae0068745675aeaf168a990985ac42ec9689ccc3
Ken Morton Jr <kmorton15@gmail.com>
2020-03-09 23:45:42 -0400

    added 'about us' description and improved layout in about component

--- components/sections/About.vue
+++ components/sections/About.vue
@@ -4,35 +4,31 @@
       <div class="columns is-centered">
         <div class="column has-text-centered">
           <p class="is-size-3 stripes">
-            <span class="bb"><b>Serving Worcester since 2001</b></span>
+            <span class="bb"
+              ><b>Serving Worcester since {{ yearFounded }}</b></span
+            >
           </p>
           <p class="is-size-5">Enter a Stranger, leave as a friend</p>
         </div>
       </div>
 
-      <div id="aboutDescription" class="columns is-centered is-vcentered">
-        <div class="column has-text-centered">
-          <section class="section">
-            <figure class="image is-16by9">
-              <img src="../../static/img/nerbTeam.jpeg" href="Our team" />
-            </figure>
-          </section>
+      <div id="aboutDescription" class="columns is-desktop is-multiline">
+        <div class="column is-half-desktop has-text-centered">
+          <figure class="image is-16by9">
+            <img src="../../static/img/nerbTeam.jpeg" href="Our team" />
+          </figure>
         </div>
-        <div class="column has-text-centered">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna
-            ex, tempus eu rutrum non, pharetra at justo. Morbi mattis risus in
-            velit facilisis suscipit. Duis quis massa mi. Quisque odio justo,
-            egestas nec ullamcorper eget, lacinia ut urna.
+        <div class="column is-half-desktop has-text-centered">
+          <p class="is-size-5">
+            We have proudly been serving Worcester for the past
+            {{ yearsInBusiness }} years, and pride ourselves in serving the best
+            sandwiches in Worcester. Come in today and try one of our delicious
+            sandwiches.
           </p>
           <br />
-          <p>
-            Quisque in sollicitudin lacus. Nunc lobortis varius lacus, nec
-            egestas nisi luctus eu. Suspendisse eros urna, bibendum ac tortor a,
-            pulvinar mollis mi. Fusce non enim at ligula semper vulputate. Sed
-            vulputate tempus erat ac lobortis. Phasellus non imperdiet eros.
-            Interdum et malesuada fames ac ante ipsum primis in faucibus. In
-            eget viverra dui.
+          <p class="is-size-5">
+            We also offer catering services, and can accommodate all of your
+            needs. We only use the freshest ingredients in our cooking.
           </p>
         </div>
       </div>
@@ -42,7 +38,17 @@
 
 <script>
 export default {
-  name: 'About'
+  name: 'About',
+  data() {
+    return {
+      yearFounded: 2001
+    }
+  },
+  computed: {
+    yearsInBusiness() {
+      return new Date(Date.now()).getUTCFullYear() - this.yearFounded
+    }
+  }
 }
 </script>
 
